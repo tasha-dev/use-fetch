4e70b95ac6e8ed209b788407fb1bb36db8547100
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2025-12-27 12:22:15 +0330

    added some comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,15 @@
+// Codes by mahdi tasha
+// Importing part
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+// Defining configs of eslint
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
+// Exporting the config as default
 export default eslintConfig;
